4f1a118a708242b3ee365ab4bcc47721b276d4cf
Opionz <ope.ajayi@hotmail.com>
2021-11-01 03:09:10 +0100

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kindly insert any word ");
+            string word = Console.ReadLine();
+            string output = ReverseString(word);
+
+            Console.WriteLine($"The reverse of the word \"{word }\" is {output}");
+        }
+
+        static string ReverseString(string word)
+        {
+            string result = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+
+                result += word[i];
+
+            }
+            return result;
+        }
+    }
+}
